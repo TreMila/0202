6d201131a05df63d55374478a97c801c2f658a7d
TreMila <wanpenglu421@163.com>
2023-02-14 16:15:48 +0800

    new

--- haha.py
+++ haha.py
@@ -6,5 +6,6 @@ def quick_sort(arr):
         pivot = arr[0]
         less = [i for i in arr[1:] if i <= pivot]
         greater = [i for i in arr[1:] if i > pivot]
+        print('aaaaa')
         return quick_sort(less) + [pivot] + quick_sort(greater)
         
